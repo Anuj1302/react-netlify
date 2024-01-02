602776afe3cf31fba03d76d3ca368c0fa30ff627
Anuj Pal <101819684+Anuj1302@users.noreply.github.com>
2024-01-02 21:42:54 +0530

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+export default function Main(){
+    return (
+        <main>
+            <h1 className="main-head">Fun Facts About React</h1>
+            <ul className="main-list">
+                <li >Was first Released in 2013</li>
+                <li >Was Originally created by Jordan Walke</li>
+                <li>Has well known over 100k Stars on Github</li>
+                <li >Is Maintained by Facebook</li>
+            </ul>
+        </main>
+    );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export default function Navbar(){
+    return (
+        <div className="navigation-bar">
+            <img className="logo-img" src="/react-logo.png"/>
+            <h2 className="nav-head">React Facts</h2>
+            <h4 className="nav-elem">React Course-Project 1</h4>
+        </div>
+    );
+}
